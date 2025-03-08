1cd0f7b8bdb6e3cbb15576e1c25c431085e60821
saputhebeast <sapumalwijekoon4@gmail.com>
2025-03-09 00:37:46 +0530

    Feat: application messages

---
+++ src/main/java/com/garage/vehicle_owner_service/core/constant/ApplicationMessages.java
@@ -0,0 +1,20 @@
+package com.garage.vehicle_owner_service.core.constant;
+
+public final class ApplicationMessages {
+
+	private ApplicationMessages() {
+	}
+
+	public static final String ERROR_DESERIALIZATION_FAILED = "api.error.serialization.failed";
+
+	public static final String ERROR_SERIALIZATION_FAILED = "api.error.deserialization.failed";
+
+	public static final String ERROR_VEHICLE_OWNER_NOT_FOUND = "api.error.vehicle-owner.not-found";
+
+	public static final String ERROR_ROLE_NOT_FOUND = "api.error.role.not-found";
+
+	public static final String ERROR_ACCESS_DENIED = "api.error.access-denied";
+
+	public static final String ERROR_VEHICLE_NOT_FOUND = "api.error.vehicle.not-found";
+
+}
