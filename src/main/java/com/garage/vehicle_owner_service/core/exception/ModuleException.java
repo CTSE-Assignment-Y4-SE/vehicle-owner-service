99a8ec74d5b931553547f3f71ec9098cb8d30585
saputhebeast <sapumalwijekoon4@gmail.com>
2025-03-09 00:42:25 +0530

    Feat: exception

---
+++ src/main/java/com/garage/vehicle_owner_service/core/exception/ModuleException.java
@@ -0,0 +1,9 @@
+package com.garage.vehicle_owner_service.core.exception;
+
+public class ModuleException extends RuntimeException {
+
+	public ModuleException(String message) {
+		super(message);
+	}
+
+}
