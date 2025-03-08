8aa918ce772abf0f79ffd0fc2d714733255306d9
saputhebeast <sapumalwijekoon4@gmail.com>
2025-03-09 00:40:39 +0530

    Feat: test file

---
+++ src/test/java/com/garage/vehicle_owner_service/VehicleOwnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garage.vehicle_owner_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleOwnerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
